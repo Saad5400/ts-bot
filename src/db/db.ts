c1d9b49319c5ef391375f1518aef0b908b73fa34
Saad5400 <sdbtwa@gmail.com>
2024-09-25 23:48:15 +0300

    Revert "converted to javascript"
    This reverts commit 3bada982901cf3f1d2fb5482897af9ba0a1dd383.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,6 @@
 import {drizzle} from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import * as schema from './schema.js';
+import * as schema from '@/db/schema.js';
 
 export const sqlite = new Database('db.sqlite');
 export const db = drizzle(sqlite, {
