085cddad697bda5d18431b6b3f584e6644361c46
Saad5400 <sdbtwa@gmail.com>
2024-09-25 20:35:39 +0300

    migrations

---
+++ src/db/schema.ts
@@ -0,0 +1,26 @@
+import {text, integer, sqliteTable} from "drizzle-orm/sqlite-core";
+import {relations} from "drizzle-orm";
+
+export const accounts = sqliteTable('accounts', {
+    id: integer('id').primaryKey({autoIncrement: true}),
+    userId: integer('user').notNull(),
+    name: text('name').notNull(),
+});
+
+export const accountsRelations = relations(accounts, ({one, many}) => ({
+    transactions: many(transactions),
+}))
+
+export const transactions = sqliteTable('transactions', {
+    id: integer('id').primaryKey({autoIncrement: true}),
+    accountId: integer('account').notNull().references(() => accounts.id),
+    amount: integer('amount').notNull(),
+    date: text('date').notNull(),
+});
+
+export const transactionsRelations = relations(transactions, ({one, many}) => ({
+    account: one(accounts, {
+        fields: [transactions.accountId],
+        references: [accounts.id],
+    }),
+}))
