ae21d49d512ae06865efe5f32862361f35005d95
Saad5400 <sdbtwa@gmail.com>
2024-09-30 07:40:25 +0300

    perfect ... or is it?

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,30 +1,17 @@
 import {text, integer, sqliteTable} from "drizzle-orm/sqlite-core";
 import {relations} from "drizzle-orm";
 
-export const accounts = sqliteTable('accounts', {
+export enum TaskStatus {
+    Pending = 'pending',
+    Completed = 'completed',
+    Cancelled = 'cancelled',
+}
+
+export const tasks = sqliteTable('tasks', {
     id: integer('id').primaryKey({autoIncrement: true}),
-    userId: integer('user').notNull(),
-    name: text('name').notNull(),
+    chatId: integer('chatId').notNull(),
+    title: text('title').notNull(),
+    status: text('status', {
+        enum: [TaskStatus.Pending, TaskStatus.Completed, TaskStatus.Cancelled],
+    }).notNull().default(TaskStatus.Pending),
 });
-
-export const accountsRelations = relations(accounts, ({one, many}) => ({
-    transactions: many(transactions),
-}))
-
-export const transactions = sqliteTable('transactions', {
-    id: integer('id').primaryKey({autoIncrement: true}),
-    accountId: integer('account').notNull().references(() => accounts.id, {
-        onDelete: "cascade",
-    }),
-    amount: integer('amount').notNull(),
-    description: text('description'),
-    category: text('category').notNull(),
-    date: text('date').notNull(),
-});
-
-export const transactionsRelations = relations(transactions, ({one, many}) => ({
-    account: one(accounts, {
-        fields: [transactions.accountId],
-        references: [accounts.id],
-    }),
-}))
--- src/server.ts
+++ src/server.ts
@@ -1,197 +1,88 @@
-import TelegramBot, {BotCommand, Message} from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
 import {db} from "@/db/db.js";
-import {accounts, transactions} from "@/db/schema.js";
-import {eq} from "drizzle-orm";
+import {tasks, TaskStatus} from "@/db/schema.js";
+import {and, eq} from "drizzle-orm";
 
-const token: string = '7746857100:AAGflmZ465_qZciGI2PCvhJS5Y5-JCHZxLU';
+const token: string = '7769069096:AAHtLrVNMtwSf6MG8dVQO6XIJcNQzfgLQSM';
 const bot = new TelegramBot(token, {polling: true});
 
-const commands: BotCommand[] = [
-    {command: 'create_account', description: '{اسم الحساب} - إنشاء حساب جديد'},
-    {command: 'list_accounts', description: '- عرض الحسابات'},
-    {command: 'delete_account', description: '{اسم الحساب} - حذف حساب'},
-    {command: 'add_transaction', description: '{التصنيف} {المبلغ} {الوصف} - إضافة عملية جديدة'},
-    {command: 'list_transactions', description: '- عرض العمليات'},
-    {command: 'delete_transaction', description: '{رقم العملية} - حذف عملية'},
-];
-bot.setMyCommands([
-    {command: 'start', description: 'عرض الأوامر المتاحة'},
-    ...commands,
-]);
-
 bot.onText(/\/start/, async (msg) => {
-    const commandsList = commands.map(command => `/${command.command}: ${command.description}`).join('\n');
-    await bot.sendMessage(msg.chat.id, `مرحباً بك في بوت المحفظة لتتبع المصاريف\n\n${commandsList}`);
+    await bot.sendMessage(msg.chat.id, 'Hello, world!');
 });
 
-bot.onText(/\/create_account$/, async (msg) => {
-    await bot.sendMessage(msg.chat.id, 'خطأ: يجب إدخال اسم الحساب بعد الأمر');
-});
-bot.onText(/\/create_account (.+)/, async (msg, match) => {
-    const accountName = match![1]!;
+bot.onText(/[ا|إ|أ]ضافة مهمة (.+)/, async (msg, match) => {
+    const title = match![1]!;
 
-    await db.insert(accounts).values({
-        userId: msg.from!.id,
-        name: accountName,
+    await db.insert(tasks).values({
+        title: title,
+        chatId: msg.chat.id,
     });
 
-    await bot.sendMessage(msg.chat.id, 'تم إنشاء الحساب بنجاح');
+    await bot.sendMessage(msg.chat.id, 'تمت إضافة المهمة');
 });
 
-bot.onText(/\/list_accounts/, async (msg) => {
-    const userAccounts = await db.query.accounts.findMany({
-        where: (account) => eq(account.userId, msg.from!.id),
-        with: {
-            transactions: true,
-        }
+bot.onText(/عرض المهام/, async (msg) => {
+    const taskList = await db.query.tasks.findMany({
+        where: (task) => and(eq(task.chatId, msg.chat.id), eq(task.status, TaskStatus.Pending)),
     })
 
-    const accountsList = userAccounts.map(function (account) {
-        // calculate total income and expenses for the past month
-        const now = new Date();
-        const monthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
-        const transactions = account.transactions.filter(transaction => new Date(transaction.date) >= monthAgo);
-        const totalIncome = transactions.filter(transaction => transaction.amount > 0).reduce((total, transaction) => total + transaction.amount, 0);
-        const totalExpenses = transactions.filter(transaction => transaction.amount < 0).reduce((total, transaction) => total + transaction.amount, 0);
-        const currentBalance = account.transactions.reduce((total, transaction) => total + transaction.amount, 0);
-
-        return `${account.name}
-الرصيد الحالي: ${currentBalance}
-الإيرادات الشهرية: ${totalIncome}
-المصاريف الشهرية: ${totalExpenses}
-`;
-    }).join('\n');
-
-    if (accountsList) {
-        await bot.sendMessage(msg.chat.id, accountsList);
-    } else {
-        await bot.sendMessage(msg.chat.id, 'ليس لديك حسابات');
-    }
-});
-
-bot.onText(/\/delete_account$/, async (msg) => {
-    await bot.sendMessage(msg.chat.id, 'خطأ: يجب إدخال اسم الحساب بعد الأمر');
-});
-bot.onText(/\/delete_account (.+)/, async (msg, match) => {
-    const accountName = match![1]!;
-
-    const userAccounts = await db.query.accounts.findMany({
-        where: (account) => eq(account.userId, msg.from!.id),
-    });
-    const accountToDelete = userAccounts.find(account => account.name === accountName);
-
-    if (!accountToDelete) {
-        await bot.sendMessage(msg.chat.id, 'خطأ: الحساب غير موجود');
+    if (taskList.length === 0) {
+        await bot.sendMessage(msg.chat.id, 'لا يوجد مهام');
         return;
     }
 
-    await db.delete(accounts).where(eq(accounts.id, accountToDelete.id));
-
-    await bot.sendMessage(msg.chat.id, 'تم حذف الحساب بنجاح');
-});
-
-bot.onText(/\/add_transaction$/, async (msg) => {
-    await bot.sendMessage(msg.chat.id, 'خطأ: يجب إدخال التصنيف والمبلغ والوصف بعد الأمر');
-});
-bot.onText(/\/add_transaction (.+?) (-?\d+)/, async (msg, match) => {
-    const category = match![1]!;
-    const amount = parseInt(match![2]!);
-    const description = match![3];
-
-    const userAccounts = await db.query.accounts.findMany({
-        where: (account) => eq(account.userId, msg.from!.id),
+    let message = '';
+    taskList.forEach((task, index) => {
+        message += `${index}. ${task.title}\n`;
     });
-    if (userAccounts.length === 0) {
-        await bot.sendMessage(msg.chat.id, 'خطأ: ليس لديك حسابات');
+
+    await bot.sendMessage(msg.chat.id, message);
+});
+
+bot.onText(/[ا|إ|أ]لغاء مهمة (\d+)/, async (msg, match) => {
+    const index = parseInt(match![1]!);
+    const taskList = await db.query.tasks.findMany({
+        where: (task) => eq(task.chatId, msg.chat.id),
+    });
+
+    if (taskList.length === 0) {
+        await bot.sendMessage(msg.chat.id, 'لا يوجد مهام');
         return;
     }
 
-    const accountPrompt = await bot.sendMessage(msg.chat.id,
-        `سيتم إضافة العملية للحساب التالي: ${userAccounts[0]!.name} بعد 10 ثواني، لتغيير الحساب الرجاء الرد على هذه الرسالة باسم الحساب`);
-    const listener = bot.onReplyToMessage(msg.chat.id, accountPrompt.message_id, async (reply) => {
-        const accountName = reply.text;
-
-        const account = userAccounts.find(account => account.name === accountName);
-        if (!account) {
-            await bot.sendMessage(msg.chat.id, 'خطأ: الحساب غير موجود');
-            return;
-        }
-
-        await db.insert(transactions).values({
-            accountId: account.id,
-            amount: amount,
-            description: description,
-            category: category,
-            date: new Date().toISOString(),
-        });
-
-        await bot.sendMessage(msg.chat.id, 'تمت إضافة العملية بنجاح');
-        bot.removeReplyListener(listener);
-        clearTimeout(timeout);
-    });
-    const timeout = setTimeout(async () => {
-        bot.removeReplyListener(listener);
-        await db.insert(transactions).values({
-            accountId: userAccounts[0]!.id,
-            amount: amount,
-            description: description,
-            category: category,
-            date: new Date().toISOString(),
-        });
-
-        await bot.sendMessage(msg.chat.id, 'تمت إضافة العملية بنجاح');
-    }, 10000);
-});
-
-bot.onText(/\/list_transactions/, async (msg) => {
-    const userAccounts = await db.query.accounts.findMany({
-        where: (account) => eq(account.userId, msg.from!.id),
-        with: {
-            transactions: true,
-        }
-    });
-    if (userAccounts.length === 0) {
-        await bot.sendMessage(msg.chat.id, 'خطأ: ليس لديك حسابات');
+    if (index >= taskList.length) {
+        await bot.sendMessage(msg.chat.id, 'الرقم غير صحيح');
         return;
     }
 
-    const transactionsList = userAccounts.flatMap(account =>
-        account.transactions.map(transaction =>
-            `${transaction.id}: ${transaction.amount} ${transaction.category} ${transaction.description ? transaction.description : ''}`
-        )
-    ).join('\n');
+    const task = taskList[index]!;
+    await db.update(tasks).set({
+        status: TaskStatus.Cancelled,
+    }).where(eq(tasks.id, task.id));
 
-    if (transactionsList) {
-        await bot.sendMessage(msg.chat.id, transactionsList);
-    } else {
-        await bot.sendMessage(msg.chat.id, 'ليس لديك أي عمليات');
-    }
+    await bot.sendMessage(msg.chat.id, 'تم إلغاء المهمة');
 });
 
-bot.onText(/\/delete_transaction$/, async (msg) => {
-    await bot.sendMessage(msg.chat.id, 'خطأ: يجب إدخال رقم العملية بعد الأمر');
-});
-bot.onText(/\/delete_transaction (\d+)/, async (msg, match) => {
-    const transactionId = parseInt(match![1]!);
-
-    const userAccounts = await db.query.accounts.findMany({
-        where: (account) => eq(account.userId, msg.from!.id),
-        with: {
-            transactions: true,
-        }
+bot.onText(/[ا|إ|أ]كمال مهمة (\d+)/, async (msg, match) => {
+    const index =  parseInt(match![1]!);
+    const taskList = await db.query.tasks.findMany({
+        where: (task) => eq(task.chatId, msg.chat.id),
     });
-    if (userAccounts.length === 0) {
-        await bot.sendMessage(msg.chat.id, 'خطأ: ليس لديك حسابات');
+
+    if (taskList.length === 0) {
+        await bot.sendMessage(msg.chat.id, 'لا يوجد مهام');
         return;
     }
 
-    const transactionToDelete = userAccounts[0]!.transactions.find(transaction => transaction.id === transactionId);
-    if (!transactionToDelete) {
-        await bot.sendMessage(msg.chat.id, 'خطأ: العملية غير موجودة');
+    if (index >= taskList.length) {
+        await bot.sendMessage(msg.chat.id, 'الرقم غير صحيح');
         return;
     }
 
-    await db.delete(transactions).where(eq(transactions.id, transactionToDelete.id));
+    const task = taskList[index]!;
+    await db.update(tasks).set({
+        status: TaskStatus.Completed,
+    }).where(eq(tasks.id, task.id));
 
-    await bot.sendMessage(msg.chat.id, 'تم حذف العملية بنجاح');
+    await bot.sendMessage(msg.chat.id, 'تم الانتهاء من المهمة');
 });
